6b9fca9833e037630a4023f62842d53ce5b97ff9
dragana-u <draganausovic2@gmail.com>
2024-02-11 12:06:38 +0100

    Kolokviusmka

---
+++ src/VtorKolokvium/v3/MostFrequentSubstring.java
@@ -0,0 +1,136 @@
+package VtorKolokvium.v3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+class MapEntry<K extends Comparable<K>,E> implements Comparable<K> {
+
+    K key;
+    E value;
+
+    public MapEntry (K key, E val) {
+        this.key = key;
+        this.value = val;
+    }
+
+    public int compareTo (K that) {
+        @SuppressWarnings("unchecked")
+        MapEntry<K,E> other = (MapEntry<K,E>) that;
+        return this.key.compareTo(other.key);
+    }
+
+    public String toString () {
+        return "(" + key + "," + value + ")";
+    }
+}
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class CBHT<K extends Comparable<K>, E> {
+
+    private SLLNode<MapEntry<K,E>>[] buckets;
+
+    @SuppressWarnings("unchecked")
+    public CBHT(int m) {
+        buckets = (SLLNode<MapEntry<K,E>>[]) new SLLNode[m];
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    public SLLNode<MapEntry<K,E>> search(K targetKey) {
+        int b = hash(targetKey);
+        for (SLLNode<MapEntry<K,E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (targetKey.equals(((MapEntry<K, E>) curr.element).key))
+                return curr;
+        }
+        return null;
+    }
+
+    public void insert(K key, E val) {		// Insert the entry <key, val> into this CBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        for (SLLNode<MapEntry<K,E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (key.equals(((MapEntry<K, E>) curr.element).key)) {
+                curr.element = newEntry;
+                return;
+            }
+        }
+        buckets[b] = new SLLNode<MapEntry<K,E>>(newEntry, buckets[b]);
+    }
+
+    public void delete(K key) {
+        int b = hash(key);
+        for (SLLNode<MapEntry<K,E>> pred = null, curr = buckets[b]; curr != null; pred = curr, curr = curr.succ) {
+            if (key.equals(((MapEntry<K,E>) curr.element).key)) {
+                if (pred == null)
+                    buckets[b] = curr.succ;
+                else
+                    pred.succ = curr.succ;
+                return;
+            }
+        }
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            for (SLLNode<MapEntry<K,E>> curr = buckets[i]; curr != null; curr = curr.succ) {
+                temp += curr.element.toString() + " ";
+            }
+            temp += "\n";
+        }
+        return temp;
+    }
+
+}
+
+public class MostFrequentSubstring {
+    public static void main (String[] args) throws IOException {
+        CBHT<String,Integer> tabela = new CBHT<String,Integer>(300);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine().trim();
+        Map<String,Integer> frequency = new HashMap<>();
+        for(int i=0;i<word.length();i++){
+            for(int j=i+1;j<=word.length();j++){
+                String substring = word.substring(i,j);
+                frequency.computeIfPresent(substring,(s, integer) -> integer+1);
+                frequency.putIfAbsent(substring,1);
+            }
+        }
+        String max = word;
+        for (Map.Entry<String, Integer> stringIntegerEntry : frequency.entrySet()) {
+            if(stringIntegerEntry.getValue() > frequency.get(max)){
+                max = stringIntegerEntry.getKey();
+            }else if(stringIntegerEntry.getValue() == frequency.get(max).intValue()){
+                if(stringIntegerEntry.getKey().length() > max.length()){
+                    max = stringIntegerEntry.getKey();
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
+
